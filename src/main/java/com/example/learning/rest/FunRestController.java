5a4cde306089a9b831dc04f97699d08154502c0d
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2026-06-24 20:07:36 +0100

    first commit

---
+++ src/main/java/com/example/learning/rest/FunRestController.java
@@ -0,0 +1,37 @@
+package com.example.learning.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "coach: " + coachName + ", Team name:" + teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world";
+    }
+
+    @GetMapping("/workout")
+    public String GetWorkout() {
+        return "Hello workout";
+    }
+
+    @GetMapping("/test")
+    public String GetTest() {
+        return "Hello test";
+    }
+}
+
